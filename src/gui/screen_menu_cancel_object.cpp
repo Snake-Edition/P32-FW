prusa3d/Prusa-Firmware-Buddy

commit 636c23f178167012454d6a9a73deb8343f5eb451

MenuItemSwitch: Migrate cancel object
BFW-6217

src/gui/screen_menu_cancel_object.cpp
@@ -23,10 +23,13 @@ void MI_CO_CANCEL_OBJECT::click(IWindowMenu & /*window_menu*/) {
     Screens::Access()->Open(ScreenFactory::Screen<ScreenMenuCancelObject>);
 }
 
+static constexpr const char *cancel_object_n_values[] = {
+    N_("Printing"),
+    N_("Canceled"),
+};
+
 MI_CO_OBJECT_N::MI_CO_OBJECT_N(int ObjectId_)
-    : WI_SWITCH_t(
-        0, string_view_utf8::MakeRAM((uint8_t *)label_buffer), nullptr,
-        is_enabled_t::yes, is_hidden_t::yes, _(str_printing), _(str_canceled))
+    : MenuItemSwitch({}, cancel_object_n_values)
     , ObjectId(ObjectId_) {
 
     UpdateName();
@@ -63,7 +66,7 @@ void MI_CO_OBJECT_N::UpdateName() {
         // Switch from object name to backup_label
         if (empty && !backup_label_used) {
             char temporary_buffer[sizeof(label_buffer) / sizeof(label_buffer[0])];
-            _(backup_label).copyToRAM(temporary_buffer, std::size(temporary_buffer));
+            _("Object %i").copyToRAM(temporary_buffer, std::size(temporary_buffer));
             snprintf(label_buffer, std::size(label_buffer), temporary_buffer, ObjectId); // Source string backup_label needs to have exactly one %i
             backup_label_used = true;
             Invalidate(); // The string memory is the same, so it needs to be invalidated manually

src/gui/screen_menu_cancel_object.hpp
@@ -14,15 +14,11 @@
 /**
  * @brief Prototype object that can hide functionality in .cpp.
  */
-class MI_CO_OBJECT_N : public WI_SWITCH_t<2> {
+class MI_CO_OBJECT_N : public MenuItemSwitch {
     char label_buffer[marlin_vars_t::CANCEL_OBJECT_NAME_LEN] = {}; ///< Buffer for object name, start empty
 
     /// True if object name form G-code is valid, false if backup_label is used or if object name changed
     bool backup_label_used = false; ///< Write backup on first call to UpdateName()
-    constexpr static const char *backup_label = N_("Object %i"); ///< "Object <number>", needs to have exactly one "%i" in it
-
-    constexpr static const char *str_printing = N_("Printing"); ///< Object is being printed as normal
-    constexpr static const char *str_canceled = N_("Canceled"); ///< Object was canceled and is skipped during print
 
     const int ObjectId; ///< Id of object to cancel with this switch
 

Snake-Edition/P32-FW

commit 380b5ac5fbeb6fa232b8a645a74ed903ade11e5d

Fix displaying empty names in cancel object menu
This fixes a regression introduced in 636c23f178167012454d6a9a73deb8343f5eb451

BFW-6680

src/gui/screen_menu_cancel_object.cpp
@@ -29,7 +29,7 @@ static constexpr const char *cancel_object_n_values[] = {
 };
 
 MI_CO_OBJECT_N::MI_CO_OBJECT_N(int ObjectId_)
-    : MenuItemSwitch({}, cancel_object_n_values)
+    : MenuItemSwitch(string_view_utf8::MakeRAM(label_buffer), cancel_object_n_values)
     , ObjectId(ObjectId_) {
 
     UpdateName();