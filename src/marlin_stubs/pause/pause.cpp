prusa3d/Prusa-Firmware-Buddy

commit e8910ddd3fd162d862ed7a3701faa4eb48f6be69

iX: Remove non-blocking load and fix the transitions
BFW-6391

src/marlin_stubs/pause/pause.cpp
@@ -249,12 +249,14 @@ Pause &Pause::Instance() {
 }
 
 bool Pause::is_unstoppable() const {
+    if constexpr (!option::has_human_interactions) {
+        return true;
+    }
+
     switch (load_type) {
     case LoadType::load:
         return FSensors_instance().HasMMU();
     case LoadType::load_to_gears:
-        return !option::has_human_interactions;
-    case LoadType::non_blocking_load:
     case LoadType::filament_change:
     case LoadType::filament_stuck:
         return true;
@@ -274,7 +276,6 @@ LoadUnloadMode Pause::get_load_unload_mode() {
     case Pause::LoadType::load:
     case Pause::LoadType::autoload:
     case Pause::LoadType::load_to_gears:
-    case Pause::LoadType::non_blocking_load:
         return LoadUnloadMode::Load;
     case Pause::LoadType::load_purge:
         return LoadUnloadMode::Purge;
@@ -297,8 +298,9 @@ bool Pause::should_park() {
     case Pause::LoadType::load_purge:
         return false;
     case Pause::LoadType::load_to_gears:
-    case Pause::LoadType::non_blocking_load:
         return !FSensors_instance().has_filament_surely(LogicalFilamentSensor::extruder);
+    case Pause::LoadType::load:
+        return option::has_human_interactions || !FSensors_instance().has_filament_surely(LogicalFilamentSensor::extruder);
     default:
         return true;
     }
@@ -399,7 +401,6 @@ void Pause::start_process([[maybe_unused]] Response response) {
     case LoadType::load:
     case LoadType::autoload:
     case LoadType::load_to_gears:
-    case LoadType::non_blocking_load:
     case LoadType::load_purge:
         set(LoadState::load_start);
         break;
@@ -452,23 +453,15 @@ void Pause::load_start_process([[maybe_unused]] Response response) {
 
     switch (load_type) {
     case LoadType::load_to_gears:
-#if HAS_SIDE_FSENSOR()
-        set_timed(LoadState::assist_insertion);
-        break;
-#else
-        set(LoadState::load_to_gears);
-#endif
-        break;
-    case LoadType::non_blocking_load:
-        // If we are certain that filament has been inserted, we can skip after load_to_gears phase
-        if (FSensors_instance().has_filament_surely(LogicalFilamentSensor::extruder)) {
-            set(LoadState::move_to_purge);
-        } else if constexpr (!option::has_side_fsensor) {
-            set(LoadState::load_to_gears);
-#if HAS_SIDE_FSENSOR()
+        // Both parts of the condition below need to be true:
+        // XL has side sensor, but not having gears FS means the "else" workflow is needed.
+        // MK4 with MMU rework doesn't have side sensor (for this case, the unofficial
+        // non-ergonomic autoload workflow would be: user pushes filament into the extruder
+        // and activates the FS by hand by moving the lever to trigger the autoload).
+        if (option::has_side_fsensor && settings.extruder_mmu_rework) {
+            set_timed(LoadState::assist_insertion);
         } else {
-            set_timed(LoadState::await_filament);
-#endif
+            set(LoadState::load_to_gears);
         }
         break;
     case LoadType::autoload:
@@ -481,13 +474,16 @@ void Pause::load_start_process([[maybe_unused]] Response response) {
     case LoadType::load_purge:
         set(LoadState::wait_temp);
         break;
-
     default:
-#if HAS_SIDE_FSENSOR()
-        set_timed(LoadState::await_filament);
-#else
-        set(LoadState::filament_push_ask);
-#endif
+        if (option::has_side_fsensor && settings.extruder_mmu_rework) {
+            if (FSensors_instance().has_filament_surely(LogicalFilamentSensor::extruder)) {
+                set(LoadState::move_to_purge);
+            } else {
+                set_timed(LoadState::await_filament);
+            }
+        } else {
+            set(LoadState::filament_push_ask);
+        }
         break;
     }
 }
@@ -528,10 +524,6 @@ void Pause::filament_push_ask_process(Response response) {
 
             if (FSensors_instance().has_filament_surely(LogicalFilamentSensor::extruder)) {
                 set(LoadState::load_to_gears);
-#if HAS_SIDE_FSENSOR()
-            } else if constexpr (option::has_side_fsensor) {
-                set_timed(LoadState::await_filament);
-#endif
             }
         }
     }
@@ -541,7 +533,6 @@ void Pause::filament_push_ask_process(Response response) {
     }
 }
 
-#if HAS_SIDE_FSENSOR()
 void Pause::await_filament_process([[maybe_unused]] Response response) {
     setPhase(PhasesLoadUnload::Inserting_stoppable);
     // If EXTRUDER sensor is not assigned or not working, or if the user fails to insert filament in time, show Warning and quit loading.
@@ -582,7 +573,6 @@ void Pause::assist_insertion_process([[maybe_unused]] Response response) {
 
     do_e_move_notify_progress_coldextrude(FILAMENT_CHANGE_SLOW_LOAD_FEEDRATE, FILAMENT_CHANGE_SLOW_LOAD_FEEDRATE, 10, 10);
 }
-#endif
 
 void Pause::load_to_gears_process([[maybe_unused]] Response response) { // slow load
     setPhase(is_unstoppable() ? PhasesLoadUnload::Inserting_unstoppable : PhasesLoadUnload::Inserting_stoppable, 10);
@@ -721,12 +711,6 @@ void Pause::eject_process([[maybe_unused]] Response response) {
 
     switch (load_type) {
     case LoadType::load_to_gears:
-    case LoadType::non_blocking_load:
-#if !HAS_HUMAN_INTERACTIONS()
-        // This state should be unreachable for printers without Human interaction and could be unrecoverable. We need to finish the FSM in order to not block interactions from Connect.
-        set(LoadState::_finish);
-        break;
-#endif
     case LoadType::filament_change:
     case LoadType::filament_stuck:
         set(LoadState::load_start);

src/common/marlin_server_types/client_fsm_types.h
@@ -95,9 +95,7 @@ enum class WarningType : uint32_t {
     HeatersTimeout,
     HotendTempDiscrepancy,
     NozzleTimeout,
-    #if HAS_SIDE_FSENSOR()
     FilamentLoadingTimeout,
-    #endif
     #if _DEBUG
     SteppersTimeout,
     #endif

src/gui/dialogs/window_dlg_warning.cpp
@@ -53,9 +53,7 @@ const img::Resource *warning_dialog_icon(WarningType warning_type) {
             { WarningType::HeatersTimeout, &img::exposure_times_48x48 },
             { WarningType::HotendTempDiscrepancy, &img::warning_48x48 },
             { WarningType::NozzleTimeout, &img::exposure_times_48x48 },
-#if HAS_SIDE_FSENSOR()
             { WarningType::FilamentLoadingTimeout, &img::warning_48x48 },
-#endif
 #if _DEBUG
             { WarningType::SteppersTimeout, &img::exposure_times_48x48 },
 #endif

src/marlin_stubs/include/pause_stubbed.hpp
@@ -39,10 +39,8 @@ protected:
         unload_finish_or_change,
         load_start,
         filament_push_ask, // must be one phase because of button click
-#if HAS_SIDE_FSENSOR()
         await_filament,
         assist_insertion,
-#endif
         load_to_gears,
         move_to_purge,
         wait_temp,
@@ -129,7 +127,6 @@ public:
         load,
         autoload,
         load_to_gears,
-        non_blocking_load,
         load_purge,
         unload,
         unload_confirm,
@@ -181,10 +178,8 @@ private:
     void unload_finish_or_change_process(Response response);
     void load_start_process(Response response);
     void filament_push_ask_process(Response response);
-#if HAS_SIDE_FSENSOR()
     void await_filament_process(Response response);
     void assist_insertion_process(Response response);
-#endif
     void load_to_gears_process(Response response);
     void move_to_purge_process(Response response);
     void wait_temp_process(Response response);
@@ -212,10 +207,8 @@ private:
             { LoadState::unload_finish_or_change, &Pause::unload_finish_or_change_process },
             { LoadState::load_start, &Pause::load_start_process },
             { LoadState::filament_push_ask, &Pause::filament_push_ask_process },
-#if HAS_SIDE_FSENSOR()
             { LoadState::await_filament, &Pause::await_filament_process },
             { LoadState::assist_insertion, &Pause::assist_insertion_process },
-#endif
             { LoadState::load_to_gears, &Pause::load_to_gears_process },
             { LoadState::move_to_purge, &Pause::move_to_purge_process },
             { LoadState::wait_temp, &Pause::wait_temp_process },

src/marlin_stubs/pause/M701_2.cpp
@@ -113,7 +113,7 @@ void filament_gcodes::M701_no_parser(FilamentType filament_to_be_loaded, const s
 
     const bool do_resume_print = static_cast<bool>(resume_print_request) && marlin_server::printer_paused();
     // Load
-    if (load_unload(option::has_human_interactions ? (do_purge_only ? Pause::LoadType::load_purge : Pause::LoadType::load) : Pause::LoadType::non_blocking_load, settings)) {
+    if (load_unload(do_purge_only ? Pause::LoadType::load_purge : Pause::LoadType::load, settings)) {
         if (!do_resume_print) {
             M70X_process_user_response(PreheatStatus::Result::DoneHasFilament, target_extruder);
         }
@@ -340,7 +340,7 @@ void filament_gcodes::M1600_no_parser(FilamentType filament_to_be_loaded, uint8_
     settings.SetResumePoint(current_position_tmp);
 #endif
 
-    if (load_unload(PRINTER_IS_PRUSA_iX() ? Pause::LoadType::non_blocking_load : Pause::LoadType::load, settings)) {
+    if (load_unload(Pause::LoadType::load, settings)) {
         M70X_process_user_response(PreheatStatus::Result::DoneHasFilament, target_extruder);
     } else {
         M70X_process_user_response(PreheatStatus::Result::DidNotFinish, target_extruder);

src/state/printer_state.cpp
@@ -535,10 +535,8 @@ ErrCode warning_type_to_error_code(WarningType wtype) {
         return ErrCode::ERR_CONNECT_GCODE_METRICS_CONFIG_CHANGE;
     case WarningType::AccelerometerCommunicationFailed:
         return ErrCode::ERR_ELECTRO_ACCELEROMETER_COMMUNICATION_FAILED;
-#if HAS_SIDE_FSENSOR()
     case WarningType::FilamentLoadingTimeout:
         return ErrCode::CONNECT_FILAMENT_LOADING_TIMEOUT;
-#endif
     }
 
     assert(false);

Snake-Edition/P32-FW

commit 9be2b79cf32aa5176092484088a65a382a9a9bce

Fix load_to_gears being unstoppable
A bug introduced in e8910ddd3fd162d862ed7a3701faa4eb48f6be69

BFW-6946

src/marlin_stubs/pause/pause.cpp
@@ -233,14 +233,16 @@ bool Pause::is_unstoppable() const {
     switch (load_type) {
     case LoadType::load:
         return FSensors_instance().HasMMU();
-    case LoadType::load_to_gears:
+
     case LoadType::filament_change:
     case LoadType::filament_stuck:
         return true;
+
     case LoadType::autoload:
     case LoadType::load_purge:
     case LoadType::unload:
     case LoadType::unload_confirm:
+    case LoadType::load_to_gears:
     case LoadType::unload_from_gears:
         return false;
     }